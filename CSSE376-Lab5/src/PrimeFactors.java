1ebdaa00750e437ceb013b36e9afaff81faa610c
Tan <lint@rose-hulman.edu>
2011-03-26 18:42:58 -0400

    Test JUnit

---
+++ CSSE376-Lab5/src/PrimeFactors.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author Tan.
+ *         Created Mar 26, 2011.
+ */
+public class PrimeFactors {
+	
+	public static ArrayList<Integer> findPrimeFactors(int n){
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		return ans;	
+		
+		
+	}
+
+}
---
+++ CSSE376-Lab5/src/PrimeFactorsTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+/**
+ * Test exercise for Lab5
+ *
+ * @author Tan.
+ *         Created Mar 26, 2011.
+ */
+public class PrimeFactorsTest {
+	@Test
+	public void testJUnit(){
+		assertTrue("Test if JUnit works well!", true);
+	}
+}
