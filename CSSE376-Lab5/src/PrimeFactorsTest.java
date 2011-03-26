8aff6419e5882317ed7644b2e729a9acafd1c9f9
Tan <lint@rose-hulman.edu>
2011-03-26 19:08:22 -0400

    testSix

--- CSSE376-Lab5/src/PrimeFactorsTest.java
+++ CSSE376-Lab5/src/PrimeFactorsTest.java
@@ -4,51 +4,54 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-
-
 /**
  * Test exercise for Lab5
- *
- * @author Tan.
- *         Created Mar 26, 2011.
+ * 
+ * @author Tan. Created Mar 26, 2011.
  */
 public class PrimeFactorsTest {
 	@Test
-	public void testJUnit(){
+	public void testJUnit() {
 		assertTrue("Test if JUnit works well!", true);
 	}
-	
+
 	@Test
-	public void testOne(){
+	public void testOne() {
 		assertEquals(list(), PrimeFactors.findThem(1));
 	}
-	
+
 	@Test
-	public void testTwo(){
+	public void testTwo() {
 		assertEquals(list(2), PrimeFactors.findThem(2));
-		
-	}
-	
-	@Test
-	public void testThree(){
-		assertEquals(list(3), PrimeFactors.findThem(3));
-		
-	}
-	
-	@Test
-	public void testFour(){
-		assertEquals(list(2, 2), PrimeFactors.findThem(4));
-		
-	}
-	public ArrayList<Integer> list(int... ints){
-		
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-		for (int i : ints){
-			ret.add(i);
-		}
-		
-		return ret;
+
 	}
 
+	@Test
+	public void testThree() {
+		assertEquals(list(3), PrimeFactors.findThem(3));
+
+	}
+
+	@Test
+	public void testFour() {
+		assertEquals(list(2, 2), PrimeFactors.findThem(4));
+
+	}
+
+	@Test
+	public void testSix() {
+		assertEquals(list(2, 3), PrimeFactors.findThem(6));
+		
+	}
+
+	public ArrayList<Integer> list(int... ints) {
+
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (int i : ints) {
+			ret.add(i);
+		}
+
+		return ret;
+	}
 
 }
